cb85a7430aeefbaf6f40059cc3da9b26c6936a38
Shpati <Shpati@DESKTOP-Q24E2H7>
2022-08-09 21:51:41 +0200

    deleted unneccessary comments

--- mongoose.js
+++ mongoose.js
@@ -12,32 +12,6 @@ mongoose.connect(dbURL)
         console.log(err)
     })
 
-// const createNote = async (req, res, next) => {
-//     const createdNote = new Notes({
-//         title: req.body.title,
-//         content: req.body.content
-//     })
-// }
-
-// createNote is a function that creates a note
-// const createNote = async (req, res, next) => {
-
-    // this one is a temporary object by which we create
-    // new note document
-//     const createdNote = new Notes({
-//         name: req.body.title,
-//         price: req.body.price,
-//         user: req.body.id
-//     })
-//     let result
-//     try {
-//         result = await createdNote.save()
-//     } catch (error) {
-//         return res.json({ message: error })
-//     }
-//     res.json({ result })
-// }
-
 const getUsers = async (req, res, next) => {
     let users
     try {
@@ -49,12 +23,10 @@ const getUsers = async (req, res, next) => {
 }
 
 const deleteUser = async (req, res, next) =>{
-    // let userId = '62f2b1a3f5a50309501b0880'
     let userId = req.param.id
     try {
      await User.findByIdAndDelete(userId)
     } catch (err) {
-        // res.json({ message: error })
         console.log(err)
     }
     console.log("Removed user : " + userId )
